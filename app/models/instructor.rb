7e3c8a5c25bfba180943aad0bc1d0098088428d8
dr-dolce14 <dolcemascop1@gmail.com>
2020-06-05 18:09:35 -0400

    stephen and paola pair programming

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,39 @@
+require 'pry'
+
+
 class Instructor
+    attr_reader :name
+@@all = []
+
+def initialize(name)
+    @name = name
+    @@all << self
+end
+
+def self.all
+    @@all
+end
+
+def pass_student(student, test_name)
+    BoatingTest.all.select do |test|
+        if test.student == student && test.test_name == test_name 
+            test.status = "passed"
+        else
+            BoatingTest.new(student, test_name, "passed", self)
+        end
+    end
+end
+
+def fail_student(student, test_name)
+    BoatingTest.all.select do |test|
+        if test.student == student && test.test_name == test_name 
+            test.status = "failed"
+        else
+           BoatingTest.new(student, test_name, "failed", self)
+        end
+    end
+end
 
 end
+
+
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,41 @@
+require 'pry'
 class Student
+attr_reader :first_name
 
+    @@all = []
+def initialize(first_name)
+    @first_name = first_name
+    @@all << self
+end
+
+def self.all
+    @@all
+end
+
+def add_boating_test(test_name, status, instructor)
+    BoatingTest.new(self, test_name, status, instructor)
+end
+
+def self.find_student(first_name)
+    Student.all.find do |student|
+        student.first_name == first_name
+    end
+end
+
+def all_tests
+    BoatingTest.all.select do |test|
+        test.student == self
+    end
+end
+
+def passed_tests
+    self.all_tests.select do |test|
+        test.status == "passed"
+    end
+end
+
+def grade_percentage
+passed_tests.length.to_f / all_tests.length.to_f
+
+end
 end
