791a559692d1c352e0e8416369ad7129decff485
orlandofat <orlandofat@gmail.com>
2020-06-05 18:56:44 -0400

    all pass

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,15 @@
 class BoatingTest
+    @@all = []
+    attr_reader :student, :test_status, :test_name, :instructor
+    def initialize(student, test_name, test_status, instructor)
+    @student = student
+    @test_name = test_name
+    @test_status = test_status
+    @instructor = instructor
+    @@all << self
+    end 
 
+    def self.all
+        @@all
+    end
 end
