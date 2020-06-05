dd3237541e0291c7a6229b0e586847d5640123f3
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-06-05 14:03:09 -0400

    need to finish instructor.rb

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,26 @@
 class Instructor
 
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def pass_student(student, boating_test_name)
+        testtopass = BoatingTest.all.select do |boating_test|
+        if testtopass
+            testtopass(status) = "passed"
+        else
+            BoatingTest.new(student, boating_test_name, "passed", self)
+        end
+    end
+
 end
