df0e81e9c2c1469188583f20e6a2ab5299d7f863
alexpac27 <alexpac27@gmail.com>
2020-06-06 18:44:10 -0400

    passing

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -2,45 +2,43 @@ require_relative './student.rb'
 require_relative './instructor.rb'
 require 'pry'
 
-class BoatingTest
-    
-    attr_reader :student, :test_name, :test_status, :instructor
-   
-    @@all = []
+# BoatingTest class:
 
-    def initialize (student, test_name, test_status, instructor)
-        @student = student
-        @test_name = test_name
-        @test_status = test_status
-        @instructor = instructor
-        @@all << self
-    end
-    
-    # def student
-    #     @student
-    # end
+        # should initialize with Student (Object), a boating test name (String), a boating test status (String), and an Instructor (Object)
 
-    # def test_name
-    #     @test_name
-    # end
+        # BoatingTest.all returns an array of all boating tests
 
-     # def test_status
-    #     @test_status
-    # end
 
-    # def instructor
-    #     @instructor
-    # end
+        class BoatingTest
 
-    def self.all
-        @@all
-    end
+            attr_reader :student, :test_name, :instructor
+            attr_accessor :test_status
+        
+            @@all = []
+        
+            def initialize(student, test_name, test_status, instructor)
+                @student = student
+                @test_name = test_name
+                @test_status = test_status
+                @instructor= instructor
+                @@all << self
+            end
+        
+            def self.all
+                @@all
+            end
+        end
+        
 
-end
+spongebob = Student.new("Spongebob")
+patrick= Student.new("Patrick")
+ 
+puff= Instructor.new("Ms.Puff")
+krabs= Instructor.new("Mr.Krabs")
+ 
+no_crashing = spongebob.add_boating_test("Don't Crash 101", "pending", puff)
+power_steering_failure = patrick.add_boating_test("Power Steering 202", "failed", puff)
+power_steering_pass = patrick.add_boating_test("Power Steering 201", "passed", krabs)
 
-puff.pass_student("Sandy","Starfish")
 
-binding.pry
-# no_crashing = spongebob.add_boating_test("Don't Crash 101", "pending", puff)
-# power_steering_failure = patrick.add_boating_test("Power Steering 202", "failed", puff)
-# power_steering_pass = patrick.add_boating_test("Power Steering 201", "passed", krabs)
+        binding.pry
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,11 +1,26 @@
-require 'pry'
+# Student class:
+
+        # should initialize with first_name
+        
+        # Student.all should return all of the student instances
+
+        # Student#add_boating_test should initialize a new boating test with a Student (Object), a boating test name (String), a boating test status (String), and an Instructor (Object)
+
+        # Student.find_student will take in a first name and output the student (Object) with that name
+
+        # Student#grade_percentage should return the percentage of tests that the student has passed, a Float (so if a student has passed 3 / 9 tests that they've taken, this method should return the Float 33.33)
+
+
 
 class Student
-    attr_reader :first_name
-    @@all = []
 
-    def initialize (first_name)
+    attr_reader :first_name
+    # array of all the student instances
+    @@all = []
+    # pass in first_name of student
+    def initialize(first_name)
         @first_name = first_name
+        # each time student is initialized, push into @@all array
         @@all << self
     end
 
@@ -13,27 +28,24 @@ class Student
         @@all
     end
 
+    def self.find_student(beef)
+        @@all.select do |student|
+            student.first_name == beef
+        end
+    end
+    
     def add_boating_test(test_name, test_status, instructor)
-        BoatingTest.new(self,test_name, test_status, instructor)
-      end
-
-    def self.find_student(first_name) 
-        self.all.find {|name| name == first_name }
+        BoatingTest.new(self, test_name, test_status, instructor)
     end
 
-    def student_grad_percentage
-        passed_tests = BoatingTest.all.find.all {|test| test.test_status == "pass"}.count.to_f
-        total_tests = [].to_f
-        percent_passed = (passed_tests/total_tests) * 100
+    def grade_percentage
+        # (3/9.to_f)*100 #=> 33.33...
+        passed = BoatingTest.all.select {|test| test.student == self && test.test_status == "passed"}.count
+        
+        test_taken = BoatingTest.all.select {|test| test.student == self}.count
+        
+        (passed/test_taken.to_f)*100
     end
-      
+
 
 end
-
-
-
-spongebob = Student.new("Spongebob")
-patrick= Student.new("Patrick")
-
-
-binding.pry
