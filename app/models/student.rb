43d97c1936d6e650581474937b141edda592d72e
travisprol <travisjohnprol@gmail.com>
2020-06-06 13:14:33 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,24 @@
 class Student
+        attr_reader :first_name
+        @@all = []
+        def initialize(first_name)
+            @first_name = first_name
+            @@all << self
+        end
+
+        def self.all
+            @@all
+        end
+
+        def add_boating_test(name, status, instructor)
+            BoatingTest.new(self, name, status, instructor)
+        end
+
+        def self.find_stuent(student_first_name)
+            self.all.find {|student| student.first_name == student_first_name}
+        end
+
+        def grade_percentage
+        end
 
 end
