e316056489dd36063b011582b064f2f89b2a86ef
ShobhitRatan <shobhit.ratan@baruchmail.cuny.edu>
2020-09-17 12:21:32 -0400

    Done

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,31 @@
 class Instructor
+    attr_accessor :name 
 
+    @@all = [] 
+    def initialize(name) 
+        @name = name 
+        @@all << self 
+    end 
+
+    def self.all 
+        @@all 
+    end 
+
+    def pass_student(student, test_name) 
+        pass_test = BoatingTest.all.find{|test| test.student.first_name == student.first_name && test.name == test_name} 
+        if pass_test 
+            pass_test.status = "passed" 
+        else 
+            BoatingTest.new(student, test_name, "passed", self) 
+        end 
+    end 
+
+    def fail_student(student, test_name) 
+        fail_test = BoatingTest.all.find{|test| test.student.first_name == student.first_name && test.name == test_name} 
+        if fail_test 
+            fail_test.status = "failed" 
+        else  
+            BoatingTest.new(student, test_name, "failed", self) 
+        end 
+    end 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,7 +7,14 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+oliver = Student.new("Oliver Queen") 
+barry = Student.new("Barry Allen") 
 
+slade = Instructor.new("Slade Wilson") 
+eobard = Instructor.new("Eobard Thawne") 
+
+no_crashing = oliver.add_boating_test("Don't Crash 101", "pending", slade) 
+power_steering_failure = olver.add_boating_test("Power Steering 202", "failed", slade) 
+power_steering_pass = barry.add_boating_test("Power Steering 201", "passed", eobard) 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
-
